b262681bc96a6dd47158882fa9a5524260fcd39e
Anastasiia_Shevchen1 <anastasiia_shevchenko1@epam.com>
2024-11-13 08:41:32 +0200

    Task 7: Authorization (version 2)

--- bin/shop-be-js-cloudfront.ts
+++ bin/shop-be-js-cloudfront.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 
 import { ProductServiceStack } from '../lib/product-service-stack';
 import { ImportServiceStack } from '../lib/import-service-stack';
-import { AuthorizationServiceStack } from '../lib/authorization-service-stack';
+//import { AuthorizationServiceStack } from '../lib/authorization-service-stack';
 
 const app = new cdk.App();
 
@@ -13,6 +13,6 @@ const env = {
   region: process.env.CDK_DEFAULT_REGION
 };
 
-new AuthorizationServiceStack(app, 'AuthorizationServiceStack', { env });
+// new AuthorizationServiceStack(app, 'AuthorizationServiceStack', { env });
 new ProductServiceStack(app, 'ProductServiceStack', { env });
 new ImportServiceStack(app, 'ImportServiceStack', { env });
